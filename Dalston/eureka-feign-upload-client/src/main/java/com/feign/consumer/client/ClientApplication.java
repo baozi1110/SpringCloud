5b1bc65a7418a21a6bb66537e6df330353b7510c
baozi1110 <743604512@qq.com>
2019-09-03 13:07:05 +0800

    gateway

--- Dalston/eureka-feign-upload-client/src/main/java/com/feign/consumer/client/ClientApplication.java
+++ Dalston/eureka-feign-upload-client/src/main/java/com/feign/consumer/client/ClientApplication.java
@@ -1,10 +1,6 @@
 package com.feign.consumer.client;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
 
 @EnableFeignClients
 @EnableDiscoveryClient
