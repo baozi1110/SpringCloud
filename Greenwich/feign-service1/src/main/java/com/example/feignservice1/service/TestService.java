b9a96bd0f80f56f89859769037b008a6ed9a1596
baozi1110 <743604512@qq.com>
2019-09-04 15:49:56 +0800

    feign中hystrix的使用

--- Greenwich/feign-service1/src/main/java/com/example/feignservice1/service/TestService.java
+++ Greenwich/feign-service1/src/main/java/com/example/feignservice1/service/TestService.java
@@ -1,6 +1,7 @@
 package com.example.feignservice1.service;
 
 import com.example.eurekafeignapi.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,8 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestService {
 
+    @Autowired
+    private SchedualServiceHi schedualServiceHi;
+
     @GetMapping("/sayHello")
     public String sayHello(@RequestParam String name){
         return "hello feign---"+name;
+    }
+
+    @GetMapping("/hiyou")
+    public String hiyout(@RequestParam String name){
+        return schedualServiceHi.sayHiFromClientOne(name);
     }
 }
--- Greenwich/gateway-demo/src/main/java/com/example/gatewaydemo/GatewayDemoApplication.java
+++ Greenwich/gateway-demo/src/main/java/com/example/gatewaydemo/GatewayDemoApplication.java
@@ -6,24 +6,27 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@RestController
 public class GatewayDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayDemoApplication.class, args);
     }
 
-    // @Bean
-    // public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-    //     return builder.routes()
-    //             //basic proxy
-    //             .route(r -> r.path("/baidu")
-    //                     .uri("https://www.baidu.com/")
-    //             ).build();
-    // }
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r
+                        .path("/get")
+                        .filters(f -> f.addRequestHeader("Hello","World"))
+                        .uri("http://httpbin.org:80")
+                ).build();
+    }
 
     // @RequestMapping("/hystrixfallback")
     // public String hystrixfallback() {
